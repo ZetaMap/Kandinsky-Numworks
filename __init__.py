0574fe8e1bf9e8e4172d70cc82c7b90ef76e9d12
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2021-10-09 22:53:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,8 +5,9 @@ GitHub project: https://github.com/ZetaMap/Kandinsky-Numworks
 
 ######### Imports #########
 
-from sys import path
-from tkinter import Tk, Canvas, font
+from os import getcwd
+from pygame import QUIT, display as screen, image, draw, event, init
+from pygame.font import Font
 from math import floor
 
 
@@ -25,37 +26,30 @@ def get_pixel(x, y):
   """Return pixel (x, y) color"""
   type_test(x, int)
   type_test(y, int)
-
-  if Ks.FIRST_PIXEL: return (0, 0, 0)
-  Ks.get_pixel(x+3, y+20)
+  return Ks.get_pixel(x, y+Ks.TOP_SIZE)
   
 def set_pixel(x, y, color):
   """Color pixel (x, y)"""
   type_test(x, int)
   type_test(y, int)
-  type_test(color, tuple)
-
-  #if Ks.FIRST_PIXEL: Ks.window()
-  Ks.set_pixel(x+3, y+21, color)
+  type_test(color, [tuple, list])
+  Ks.set_pixel(x, y+Ks.TOP_SIZE, convert_color(color))
 
 def color(r, g, b):
   """Define a rgb color"""
   type_test(r, int)
   type_test(g, int)
   type_test(b, int)
-  
-  return Ks.convert_color((r, g, b), False)
+  return convert_color((r, g, b))
 
 def draw_string(text, x, y, color=(0,0,0), background=(255,255,255)):
   """Display a text from pixel (x, y)"""
   type_test(text, str)
   type_test(x, int)
   type_test(y, int)
-  type_test(color, tuple)
-  type_test(background, tuple)
-  
-  #if Ks.FIRST_PIXEL: Ks.window()    
-  Ks.draw_string(text, x+3, y+20, color, background)
+  type_test(color, [tuple, list])
+  type_test(background, [tuple, list])
+  Ks.draw_string(text, x, y+Ks.TOP_SIZE-2, convert_color(color), convert_color(background))
 
 def fill_rect(x, y, width, height, color):
   """Fill a rectangle at pixel (x, y)"""
@@ -63,95 +57,87 @@ def fill_rect(x, y, width, height, color):
   type_test(y, int)
   type_test(width, int)
   type_test(height, int)
-  type_test(color, tuple)
+  type_test(color, [tuple, list])
+  Ks.fill_rect(x, y+Ks.TOP_SIZE, width, height, convert_color(color))
   
-  #if Ks.FIRST_PIXEL: Ks.window()
-  Ks.fill_rect(x+3, y+21, width, height, color)
-  
-def display_(func=None):
-  if func != None: Ks.canvas.after(1, func)
-  Ks.root.mainloop()
+def display():
+  run = True
+  while run: 
+    for i in event.get():
+      if i.type == QUIT: run = False
 
 
 ######### Main class #########
 
 class KS:
-  FIRST_PIXEL = True
-  first_ground = []
+  TOP_SIZE = 19
+  SCREEN = (320, 241)
 
   def __init__(self):
-    """Create the window if the fisrt pixel is colored"""
- 
-    def default(): 
-      self.FIRST_PIXEL = True
-      self.root.quit()
+    init()
+    path = getcwd()+'\\'
+      
+    self.root, self.windowFont, self.font = screen.set_mode(self.SCREEN), Font(path+"small_font.ttf", 12), Font(path+"large_font.ttf", 16)
+    screen.set_caption("kandinsky - Numworks")
+    screen.set_icon(image.load(path+"icon.ico"))
     
-    self.root, self.windowFont, self.font = Tk(), font.Font(family="Consolas", size=10), font.Font(family="Source Pixel Large", size=12)
-    self.canvas = Canvas(self.root, bg="white", width=323, height=242)
-    self.root.title("kandinsky - Numworks")
-    self.root.iconbitmap(path+"icon.ico")
-    self.root.geometry("326x246+640+360")
-    self.root.resizable(False, False)
+    self.root.fill((255, 255, 255))
+    self.draw_content()
+    screen.update()
 
-    self.first_ground.append(self.canvas.create_rectangle(2, 2, 323, 243))
-    self.first_ground.append(self.canvas.create_rectangle(3, 3, 322, 20, fill="#ffb531", outline="#ffb531"))
-    self.first_ground.append(self.canvas.create_text(8, 3, text="deg", fill="white", font=self.windowFont, anchor="nw"))
-    self.first_ground.append(self.canvas.create_text(160, 11, text="PYTHON", fill="white", font=self.windowFont))
-    self.first_ground.append(self.canvas.create_line(302, 7, 302, 15, fill="white"))
-    self.first_ground.append(self.canvas.create_rectangle(304, 7, 313, 14, fill="white", outline="white"))
-    self.first_ground.append(self.canvas.create_line(315, 7, 315, 15, fill="white"))
-    self.first_ground.append(self.canvas.create_line(316, 9, 316, 13, fill="white"))
-    self.canvas.pack()
-    self.canvas.update()
-
-    self.root.protocol("WM_DELETE_WINDOW", default)
-    self.FIRST_PIXEL = False
-
-#  def window(self):
-#    pass
+  def draw_content(self):
+    draw.line(self.root, (0, 0, 0), (0, 0), (self.SCREEN[0], 0))
+    draw.rect(self.root, "#ffb531", (0, 1, self.SCREEN[0], 18))
+    self.root.blit(self.windowFont.render("deg", True, (255, 255, 255)), (5, 1))
+    self.root.blit(self.windowFont.render("PYTHON", True, (255, 255, 255)), (139, 2))
+    draw.line(self.root, (255, 255, 255), (300, 6), (300, 13))
+    draw.rect(self.root, (255, 255, 255), (302, 6, 10, 8))
+    draw.line(self.root, (255, 255, 255), (313, 6), (313, 13))
+    draw.line(self.root, (255, 255, 255), (314, 8), (314, 11))
 
   def get_pixel(self, x, y):
-    if x < 3 or y < 20 or x > 322 or y > 242: return (0, 0, 0)
-    return self.canvas.find_closest(x, y)
+    if y < self.TOP_SIZE: return (0, 0, 0)
+    return convert_color(self.root.get_at((x, y))[:-1])
 
   def set_pixel(self, x, y, color):
-    if x < 3 or y < 20 or x > 322 or y > 242: return
-    self.canvas.create_line(x, y, x+1, y+1, fill=self.convert_color(color))
-    self.canvas.update()
+    if y < self.TOP_SIZE: return
+
+    draw.line(self.root, color, (x, y), (x, y))
+    screen.flip()
 
   def draw_string(self, text, x, y, color, background):
-    self.canvas.create_rectangle(x, y+1, x+len(text)*10, y+19, fill=self.convert_color(background), width=0)
-    self.canvas.create_text(x, y, text=text, fill=self.convert_color(color), font=self.font, anchor="nw")
-    for i in self.first_ground: self.canvas.tag_raise(i)
-    self.canvas.update()
-    
-
+    draw.rect(self.root, background, (x, y+2, len(text)*10, 18))
+    self.root.blit(self.font.render(text, True, color), (x, y))
+    if y < self.TOP_SIZE: self.draw_content()
+    screen.flip()
+  
   def fill_rect(self, x, y, width, height, color):
-    if x < 3 or y < 20 or x > 322 or y > 242: return
-    if width == 0 or height == 0: return
-    if (x+width > 322 or x+width < 3) and width != 1: width = 323-x if x+width > 322 else 3-x
-    if (y+height > 242 or y+height < 21) and height != 1: height = 243-y if y+height > 242 else 21-y
-
-    self.canvas.create_rectangle(x, y, x+width, y+height, fill=self.convert_color(color), width=0)
-    self.canvas.update()
-
-  def convert_color(self, color, returnHex=True):
-    color = list(color)
-    if len(color) != 3: raise TypeError("Color needs 3 components")
+    if width < 0: 
+      x += width
+      width *= -1
+    if height < 0:
+      y += height
+      height *= -1
+    if y < self.TOP_SIZE and y+height > self.TOP_SIZE: 
+      height -= self.TOP_SIZE-y
+      y = self.TOP_SIZE
+    elif y+height <= self.TOP_SIZE: return
     
-    for i in range(len(color)):
-      if color[i] > 255: color[i] %= 256
-    if returnHex: return "#{0:02x}{1:02x}{2:02x}".format(
-        floor(color[0] / 8) * 8, 
-        floor(color[1] / 4) * 4,
-        floor(color[2] / 8) * 8)
-    else: return (
-        floor(color[0] / 8) * 8, 
-        floor(color[1] / 4) * 4,
-        floor(color[2] / 8) * 8)
+    draw.rect(self.root, color, (x, y, width, height))
+    screen.flip()
+
+Ks = KS()
 
 
 ######### Private method #########
+
+def convert_color(color):
+  if type(color) != list: color = list(color)
+  if len(color) != 3: raise TypeError("Color needs 3 components")
+  
+  for i in range(len(color)):
+    if color[i] > 255: color[i] %= 256
+  return (floor(color[0] / 8) * 8, floor(color[1] / 4) * 4, floor(color[2] / 8) * 8)
 
 """Function taken from module: Python_Upgrade. 
 link: https://github.com/ZetaMap/Python_Upgrade
@@ -202,27 +188,12 @@ def type_test(_object, _type, canBeNone=False, withError=True, contentException=
     else: return False
 
 
-######### Module init #########
-
-for i in path:
-  if i.endswith("site-packages"):
-    path = i+"\\kandinsky\\"
-    break
-Ks = KS()
-if not "Source Pixel Large" in font.families(Ks.root): print(
-"""RuntimeWarning: The font used by the module ('Source Pixel Large') is not installed on your system!
-/!\\ If you don't install it, the texts display will not be correct. /!\\
-To install it:
-  1- open the indicated file: {}
-  2- click on the button 'Install', at the top of the window, to install the font on your system.
-""".format(path+"font.ttf"))
-
-
 ######### Example code #########
 
 if __name__ == '__main__':
   """Source: https://my.numworks.com/python/andreanx/chromac
   """
+  
   from cmath import *
 
   def hsv2color(h, s=1, v=1):
@@ -250,13 +221,12 @@ if __name__ == '__main__':
           hsv2color((phase(z)-tred)*(1-2*rev), 
           modsv(r/rmax,ds),modsv(r/rmax,dv)))
 
-  def demo():
-    sw, sh, r = 320,220, 45
-    lx3, ly2 = [r,(sw-1)/2,sw-1-r], [r,sh-1-r]
-    lx2=[(lx3[0]+lx3[1])/2,(lx3[1]+lx3[2])/2]
+  sw, sh, r = 320,220, 45
+  lx3, ly2 = [r,(sw-1)/2,sw-1-r], [r,sh-1-r]
+  lx2=[(lx3[0]+lx3[1])/2,(lx3[1]+lx3[2])/2]
 
-    for x in range(2): chromac(lx2[x], sh/2, r, -1, not(x)*-1, x*pi/4, x%2)
-    for y in range(2):
-      for x in range(3): chromac(lx3[x], ly2[y], r, not(y), x-1, (2+3*y+x)*pi/4, (x+y)%2)
-
-  display_(demo)
+  for x in range(2): chromac(lx2[x], sh/2, r, -1, not(x)*-1, x*pi/4, x%2)
+  for y in range(2):
+    for x in range(3): chromac(lx3[x], ly2[y], r, not(y), x-1, (2+3*y+x)*pi/4, (x+y)%2)
+  
+  display()
