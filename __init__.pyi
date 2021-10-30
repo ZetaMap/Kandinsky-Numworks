0b6947849aab0af527a8562d7287cd071d02585a
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2021-10-30 17:57:27 +0200

    Add files via upload

--- __init__.pyi
+++ __init__.pyi
@@ -1,10 +1,16 @@
-from typing import Tuple, NoReturn
+from typing import Tuple, List, NoReturn
 
-_Color = Tuple[int, int, int]
+_ColorInput = Tuple[int, int, int] | List[int, int, int] | str
+_ColorOutput = Tuple[int, int, int]
 
-def get_pixel(x: int, y: int) -> _Color: pass
-def set_pixel(x: int, y: int, color: _Color) -> None: pass
-def color(r: int, g: int, b: int) -> _Color: pass
-def draw_string(text: str, x: int, y: int, color: _Color=(), background: _Color=()) -> None: pass
-def fill_rect(x: int, y: int, width: int, height: int, color: _Color) -> None: pass
+def get_pixel(x: int, y: int) -> _ColorOutput: pass
+def set_pixel(x: int, y: int, color: _ColorInput) -> None: pass
+def color(r: int, g: int, b: int) -> _ColorOutput: pass
+def draw_string(
+  text: str, 
+  x: int, 
+  y: int, 
+  color: _ColorInput=(), 
+  background: _ColorInput=()) -> None: pass
+def fill_rect(x: int, y: int, width: int, height: int, color: _ColorInput) -> None: pass
 def display() -> NoReturn: pass
