e9fe71c72c5f713a0eb4a546c4bc3f66ccc768f7
ZetaMap <56844734+ZetaMap@users.noreply.github.com>
2021-12-21 16:46:05 +0100

    bug fix

--- __main__.py
+++ __main__.py
@@ -1,2 +1,6 @@
 try: from kandinsky.util.demo import *
-except ImportError: print("Error: demo.py can't be load!")
+except ImportError as e: 
+  if e.msg.lower() == "no module named 'ion'": print("""
+>>> Ion module is not installed on your system.
+To install it, type 'pip install ion-Numworks' in a command prompt.""")
+  else: print("Error: demo.py can't be load!")
