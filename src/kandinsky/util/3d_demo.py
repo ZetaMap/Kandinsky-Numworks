65dbb6400b4ea7ce938027305766892f272af3b3
ZetaMap <zack.filmgaming@gmail.com>
2023-09-14 21:44:31 +0200

    updated 3d_demo.py

--- src/kandinsky/util/3d_demo.py
+++ src/kandinsky/util/3d_demo.py
@@ -1,12 +1,14 @@
 """
-A Start of an 3D engine for Numworks.
+A start of a 3D engine. Use``OK`` to display the faces or edges of cube and ``←↑→↓+-`` to move it.
 
+**NOTE**: This script only work on Upsilon because ``fill_polygon()`` function doesn't exist in others OS.
 Source: https://my.numworks.com/python/antarctus/cube_move
 """
 
 import os
-os.environ["KANDINSKY_OS_MODE"]='0'
-os.environ['KANDINSKY_ZOOM_RATIO'] = '2'
+if hasattr(os, "environ"):
+  os.environ["KANDINSKY_OS_MODE"]= '0'
+  os.environ['KANDINSKY_ZOOM_RATIO'] = '2'
 from math import cos,sin,pi
 from kandinsky import *
 from time import sleep
@@ -43,26 +45,8 @@ def cube3d(x1,y1,z1,x2,y2,z2):
 def points3d(XYZpoints):
   MODEL.append(XYZpoints)
 
-def line(x1,y1,x2,y2,c):
-  w=x2-x1
-  h=y2-y1
-  if abs(w)>=abs(h):
-    d=h/w
-    for i in range(0,w,(w>0)*2-1):
-      set_pixel(x1+i,y1+int(d*i+0.5),c)
-  else:
-    d=w/h
-    for i in range(0,h,(h>0)*2-1):
-      set_pixel(x1+int(d*i+0.5),y1+i,c)
-
-try:
-  line=draw_line
-except:
-  pass
-
-
 def render(cosx,sinx,cosy,siny,cosz,sinz):
-  fill_rect(0,0,320,200,(0,0,0))
+  fill_rect(0,0,320,180,(0,0,0))
 
   for i in range(len(MODEL)):
     polygon = []
@@ -79,10 +63,16 @@ def render(cosx,sinx,cosy,siny,cosz,sinz):
       polygon.append([px,py])
 
       #debug
-      draw_string(f"{px},{py}",px,py, "white", "black",True)
-
+      draw_string("%d,%d"%(px,py), px, py, "white", "black", 1)
+    draw_string("%f,%f,%f"%(cosx,cosy,cosz), 5, 0, "white", "black", 1)
+    draw_string("%f,%f,%f"%(sinx,siny,sinz), 5, 12, "white", "black", 1)
+      
+    ""
     if FILL_MODEL: fill_polygon(polygon,COLORS[i%6])
-    else: line(*polygon[0],*polygon[1],COLORS[i//2%6])
+    else:
+      x1,y1=polygon[0]
+      x2,y2=polygon[1]
+      draw_line(x1,y1,x2,y2,COLORS[i//2%6])
 
 # ???
 def xturn(y,z,cosx,sinx):
@@ -96,15 +86,14 @@ def zturn(x,y,cosz,sinz):
 
 cube3d(-100,-100,-100,100,100,100)
 
-xrot=0
-yrot=0
-zrot=0
+xrot=yrot=zrot=xoffset=yoffset=0
 
 fill_rect(0,0,320,222,(0,)*3)
-draw_string("OK: fill on/off\t\t\t\tArrows: move",5,207,"white","black",True)
+draw_string("OK: fill on/off"+(' '*13)+"Arrows/+/-: move",5,207,"white","black",True)
 while True:
   xrot+=pi/90*(keydown(KEY_UP)-keydown(KEY_DOWN))
   yrot+=pi/90*(keydown(KEY_LEFT)-keydown(KEY_RIGHT))
+  zrot+=pi/90*(keydown(KEY_MINUS)-keydown(KEY_PLUS))
 
   if keydown(KEY_OK):
     FILL_MODEL = not FILL_MODEL
@@ -113,4 +102,4 @@ while True:
     sleep(0.2)
 
   render(cos(xrot),sin(xrot),cos(yrot),sin(yrot),cos(zrot),sin(zrot))
-  sleep(0.01)
+#  sleep(0.01)
